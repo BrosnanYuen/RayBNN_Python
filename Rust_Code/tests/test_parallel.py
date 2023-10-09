85ad7055ddf2d6fe3af0749feccdcf9d39c5f808
brosnanyuen <brosnany@uvic.ca>
2023-10-08 19:11:37 -0700

    save

--- Rust_Code/tests/test_parallel.py
+++ Rust_Code/tests/test_parallel.py
@@ -1,9 +1,9 @@
 import numpy as np
-import rust_parallel
+import raybnn_python
 
 
 def test_rows_dot():
     x = np.ones((128, 1024), dtype=np.float64)
     y = np.ones((1024,), dtype=np.float64)
-    z = rust_parallel.rows_dot(x, y)
+    z = raybnn_python.rows_dot(x, y)
     np.testing.assert_array_almost_equal(z, 1024 * np.ones((128,), dtype=np.float64))

b30ac410608d4180dff2b7524d75a76fefaf691a
brosnanyuen <brosnany@uvic.ca>
2023-10-09 20:37:43 -0700

    save

--- Rust_Code/run_network.py
+++ Rust_Code/run_network.py
@@ -4,36 +4,38 @@ import raybnn_python
 
 
 
+def main():
+    dir_path = "/tmp/"
 
-dir_path = "/tmp/"
+    max_input_size = 162
+    input_size = 162
 
-max_input_size = 162
-input_size = 162
+    max_output_size = 2
+    output_size = 2
 
-max_output_size = 2
-output_size = 2
+    max_neuron_size = 600
 
-max_neuron_size = 600
-
-batch_size = 100
-traj_size = 20
-
-
-arch_search = raybnn_python.create_start_archtecture(input_size,
-max_input_size,
-output_size,
-max_output_size,
-max_neuron_size,
-batch_size,
-traj_size,
-dir_path)
+    batch_size = 100
+    traj_size = 20
 
 
-raybnn_python.print_model_info(arch_search)
+    arch_search = raybnn_python.create_start_archtecture(input_size,
+    max_input_size,
+    output_size,
+    max_output_size,
+    max_neuron_size,
+    batch_size,
+    traj_size,
+    dir_path)
+
+
+    raybnn_python.print_model_info(arch_search)
 
 
 
 
+if __name__ == '__main__':
+    main()
 
 
 

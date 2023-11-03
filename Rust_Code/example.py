fe05d34ea9850744f8cc92e3a5f223eeb9f14dd7
brosnanyuen <brosnany@uvic.ca>
2023-11-02 20:36:38 -0700

    save

--- Rust_Code/example.py
+++ Rust_Code/example.py
@@ -3,15 +3,11 @@ import raybnn_python
 
 
 
+x = np.random.rand(2,3,5).astype(np.float32)
 
 
-
-x = np.array([ [-2, 4, 7, 0.1, 5], [3, 6, 9, -1, 17], [-4, 1, 12, 8, 42]   ], dtype=np.float64)
-y = np.array([ [-22, 44], [73, 26]   ], dtype=np.float64)
 
 print(x)
-
-print(y)
 
 print("Rust")
 

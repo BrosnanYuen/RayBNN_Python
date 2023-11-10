21bacc244163e419a7d3921f0425296187446621
brosnanyuen <brosnany@uvic.ca>
2023-11-09 18:39:38 -0800

    save

--- Rust_Code/src/lib.rs
+++ Rust_Code/src/lib.rs
@@ -396,7 +396,7 @@ fn raybnn_python<'py>(_py: Python<'py>, m: &'py PyModule) -> PyResult<()> {
 				for i in 0..dim0 {
 					for j in 0..dim1 {
 						for k in 0..dim2 {
-							arr.uget_raw([i, j, k, l]).write(Yhat_vec[i + (dim0*j) + (dim0*dim1*k) + (dim0*dim1*dim2*l)]);
+							arr.uget_raw([i, j, k, l]).write(Yhat_vec[i + (dim0*j) + (dim0*dim1*k) ]);
 						}
 					}
 				}
